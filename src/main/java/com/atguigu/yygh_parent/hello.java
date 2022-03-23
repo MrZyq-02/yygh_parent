d0b572b4b9a53e50e7fcec864e12e4d2276f6b6e
Zyq <512213924@qq.com>
2022-03-24 06:43:51 +0800

    3。24

--- src/main/java/com/atguigu/yygh_parent/hello.java
+++ src/main/java/com/atguigu/yygh_parent/hello.java
@@ -12,6 +12,7 @@ public class hello {
         System.out.println("张义骞加油啊");
         System.out.println("张义骞加油啊");
         System.out.println("加油啊");
+        System.out.println("今天是3.24加油啊");
 
     }
 }
