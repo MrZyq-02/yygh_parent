b9a8afd5ebd6f90b800d9c63e04120c1d9587e1f
Zyq <512213924@qq.com>
2022-03-23 21:26:17 +0800

    hello git2

--- src/main/java/com/atguigu/yygh_parent/hello.java
+++ src/main/java/com/atguigu/yygh_parent/hello.java
@@ -3,5 +3,10 @@ package com.atguigu.yygh_parent;
 public class hello {
     public static void main(String[] args) {
         System.out.println("你好");
+        System.out.println("你好");
+
+        System.out.println("你好");
+
+
     }
 }
