9aa11d4724193f35272d205c14e63716ecf5d809
MrZyq-02 <99623005+MrZyq-02@users.noreply.github.com>
2022-03-23 21:57:17 +0800

    Update hello.java

--- src/main/java/com/atguigu/yygh_parent/hello.java
+++ src/main/java/com/atguigu/yygh_parent/hello.java
@@ -7,7 +7,7 @@ public class hello {
 
         System.out.println("你好");
         System.out.println("你好3");
-        System.out.println("又改了aaaaaaa");
+        System.out.println("又改了zhangyiqian");
 
 
     }
